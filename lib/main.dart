3bcfe50566c4e4d7793cfe38723290f38aa83273
Ravi Tomar <ravitomar7@gmail.com>
2020-02-13 21:57:15 +0530

    Completed Xylophone app, incluing expand class and understood functions in Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  Expanded putKeys({Color color, int note}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          player.play('note$note.wav');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              putKeys(color: Colors.red, note: 1),
+              putKeys(color: Colors.orange, note: 2),
+              putKeys(color: Colors.yellow, note: 3),
+              putKeys(color: Colors.green, note: 4),
+              putKeys(color: Colors.teal, note: 5),
+              putKeys(color: Colors.blue, note: 6),
+              putKeys(color: Colors.deepPurple, note: 7),
+            ],
+          ),
         ),
       ),
     );
